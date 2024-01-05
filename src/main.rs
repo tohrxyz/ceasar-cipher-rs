ca09245a02d195e61360ed79a27bd00c67289904
tohrxyz <tomas.hrib@proton.me>
2024-01-05 16:31:31 +0100

    feat: implement also decrypting input

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::env;
 // '-' starts, to be a placeholder of 0th element
 const UPPERCASE_ALPHABET: &str = "-ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWERCASE_ALPHABET: &str = "-abcdefghijklmnopqrstuvwxyz";
+const R_UPPERCASE_ALPHABET: &str = "ZYXWVUTSRQPONMLKJIHGFEDCBA-";
+const R_LOWERCASE_ALPHABET: &str = "zyxwvutsrqponmlkjihgfedcba-";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -24,7 +26,8 @@ fn main() {
         return;
     }
 
-    shift_string(&cleaned_input, &nshift)
+    let encrypted_string = encrypt_shift_string(&cleaned_input, &nshift);
+    decrypt_shift_string(&encrypted_string, &nshift);
 
 }
 
@@ -42,7 +45,7 @@ fn is_valid_alphabet_char(input: &str) -> bool {
     return is_valid;
 }
 
-fn shift_string(input: &str, nshifted: &i32) {
+fn encrypt_shift_string<'a>(input: &'a str, nshifted: &'a i32) -> String {
     let ics = input.char_indices();
     let mut shifted_text: String = String::from("");
     for (index, c) in ics.clone().into_iter() {
@@ -106,5 +109,76 @@ fn shift_string(input: &str, nshifted: &i32) {
         }
     }
     println!("Input string: {}", input);
-    println!("Shifted string: {} by {}", &shifted_text, nshifted);
+    println!("Encrypted string: {} by {}", &shifted_text, nshifted);
+    let result = shifted_text.clone();
+    return result;
+}
+
+fn decrypt_shift_string(input: &str, nshifted: &i32) -> String {
+    let ics = input.char_indices();
+    let mut shifted_text: String = String::from("");
+    for (index, c) in ics.clone().into_iter() {
+        if R_UPPERCASE_ALPHABET.contains(c) {
+            let x =  match R_UPPERCASE_ALPHABET.char_indices().find(|&x| x.1 == c.clone()) {
+                Some(some) => {
+                    some.0
+                },
+                None => { 
+                    println!("No x usize");
+                    0
+                 },
+            };
+            let next_pos = match (x + *nshifted as usize) > 26 {
+                true => {
+                    (x + *nshifted as usize) - 26
+                },
+                false => x + *nshifted as usize,
+            };
+            let char_at_shifted_position = R_UPPERCASE_ALPHABET.char_indices().nth(next_pos);
+            match char_at_shifted_position {
+                Some(char_tuple) => {
+                    shifted_text.push_str(&char_tuple.1.to_string());
+                    // println!("string [{}]: {}", index, &shifted_text);
+                },
+                None => {
+                    println!("No char at shifted position [{}]: '{}'", index, c);
+                    println!("x: {} Multiple: {}", x, (x + x.next_multiple_of(*nshifted as usize)));
+                    println!("{:?}", R_UPPERCASE_ALPHABET.chars().nth(x));
+                },
+            }
+        }
+        if R_LOWERCASE_ALPHABET.contains(c) {
+            let x =  match R_LOWERCASE_ALPHABET.char_indices().find(|&x| x.1 == c.clone()) {
+                Some(some) => {
+                    some.0
+                },
+                None => { 
+                    println!("No x usize");
+                    0
+                 },
+            };
+            let next_pos = match (x + *nshifted as usize) > 26 {
+                true => {
+                    (x + *nshifted as usize) - 26
+                },
+                false => x + *nshifted as usize,
+            };
+            let char_at_shifted_position = R_LOWERCASE_ALPHABET.char_indices().nth(next_pos);
+            match char_at_shifted_position {
+                Some(char_tuple) => {
+                    shifted_text.push_str(&char_tuple.1.to_string());
+                    // println!("string [{}]: {}", index, &shifted_text);
+                },
+                None => {
+                    println!("No char at shifted position [{}]: '{}'", index, c);
+                    println!("x: {} Multiple: {}", x, (x + x.next_multiple_of(*nshifted as usize)));
+                    println!("{:?}", R_LOWERCASE_ALPHABET.chars().nth(x));
+                },
+            }
+        }
+    }
+    println!("Input string: {}", input);
+    println!("Decrypted string: {} by {}", &shifted_text, nshifted);
+    let result = shifted_text.clone();
+    return result;
 }
